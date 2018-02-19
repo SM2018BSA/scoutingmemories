d5a039f36aa347bf3b237551c500c9f30b1e36e4
Roger Ellis <roger2014jr@gmail.com>
2018-02-18 21:35:00 -0500

    test php action on save of my form

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -12,3 +12,10 @@ function enqueue_parent_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 
+add_filter('frm_use_embedded_form_actions', 'frm_trigger_embedded_form_actions', 10, 2);
+function frm_trigger_embedded_form_actions( $trigger_actions, $args ) {
+	if ( $args['form']->id == 9 ) {
+		$trigger_actions = true;
+	}
+	return $trigger_actions;
+}
