c9c19b9b3fa1ee2113b6eac32f0c732f95333999
Roger Ellis <SM2018BSA@gmail.com>
2019-07-22 21:43:28 -0400

    fixed shortcodes

--- wp-content/themes/Scouting-Memories/inc/add-shortcodes.php
+++ wp-content/themes/Scouting-Memories/inc/add-shortcodes.php
@@ -2,13 +2,15 @@
 
 
 if (!function_exists('my_user_meta')) :
-    add_shortcode('my_user_meta', function ($atts, $content = null) {
+    function my_user_meta($atts, $content = null) {
         return esc_html(get_user_meta(get_current_user_id(), $atts['key'], true));
-    });
+    }
+    add_shortcode('my_user_meta', 'my_user_meta');
 endif;
 
 if (!function_exists('my_site_url')) :
-    add_shortcode('site_url', function ($atts = null, $content = null) {
+    function my_site_url($atts = null, $content = null) {
         return site_url();
-    });
+    }
+    add_shortcode('site_url', 'my_site_url');
 endif;
