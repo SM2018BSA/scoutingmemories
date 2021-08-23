479e54648016681b2ef5bf56df17f304ca766aac
SM2018BSA <36606567+SM2018BSA@users.noreply.github.com>
2021-08-22 21:13:16 -0400

    August 22nd Update 2

--- wp-content/themes/scoutingmemories/Classes/CurrentUser.php
+++ wp-content/themes/scoutingmemories/Classes/CurrentUser.php
@@ -53,7 +53,6 @@ class CurrentUser {
 
 
 
-
 	// caps:
 	//
 	// create_posts
--- wp-content/themes/scoutingmemories/single.php
+++ wp-content/themes/scoutingmemories/single.php
@@ -17,7 +17,7 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+            get_template_part( 'template-parts/content', get_post_type() );
 
 
 
@@ -33,10 +33,10 @@ get_header();
 
 
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
 
 		endwhile; // End of the loop.
 		?>
